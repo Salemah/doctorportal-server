e16c21cc48140b892cf8318775689ad622e1719c
Salemah <kadertanvir1@gmail.com>
2022-02-04 23:24:16 +0600

    jwt updatd

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const express = require('express');
+const admin = require("firebase-admin");
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+
+
+
+const serviceAccount = require('./dctoken.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+
+        }
+        catch {
+
+        }
+
+    }
+    next();
+
+
+
+}
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +37,11 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rojhc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -22,9 +55,9 @@ async function run() {
         const usersCollection = database.collection("users");
 
 
-        app.get('/appointments',async(req,res)=>{
+        app.get('/appointments',verifyToken,async (req, res) => {
             const email = req.query.email;
-        
+
             const date = new Date(req.query.date).toLocaleDateString();
 
             const query = { email: email, date: date }
@@ -34,19 +67,24 @@ async function run() {
             res.json(appointments);
 
         })
-      
-        app.post('/appointment',async(req,res)=>{
+
+        app.post('/appointment', async (req, res) => {
             const query = req.body;
-            const result = await appointMentCollection.insertOne(query );
+            const result = await appointMentCollection.insertOne(query);
             res.json(result)
 
         })
-      
-        app.post('/users',async(req,res)=>{
-            const user = req.body;
-            const result = await usersCollection.insertOne(user);
-            console.log(result);
-            res.json(result);
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+
+            res.json({ admin: isAdmin });
 
         })
         //
@@ -58,15 +96,28 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc, options);
             res.json(result);
         });
-        app.put('/users/admin',async (req, res) => {
+        app.put('/users/admin', verifyToken, async (req, res) => {
             const user = req.body;
-            const filter = { email: user.email };
-            const updateDoc = { $set: {role:'admin'} };
-            const result = await usersCollection.updateOne(filter, updateDoc);
-            res.json(result);
-
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'you do not have access to make admin' })
+            }
 
         });
+
+
+
+
+
 
 
     }
